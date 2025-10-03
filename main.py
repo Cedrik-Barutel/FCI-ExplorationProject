71a54ac51b78a741db8a40440b7766ddbd6caf9d
benji <bagi.benji@gmail.com>
2025-10-03 15:19:30 +0200

    o) new simulations

--- main.py
+++ main.py
@@ -2,10 +2,12 @@ import os
 import pandas as pd
 from new_code import run_simulation as simulation
 from generic_analyse_script import run_analysis as analyse  # call analysis without animations
+from new_analyse_update_290925 import run_sweeping_eff
 import gc
 import matplotlib.pyplot as plt
+import numpy as np
 
-def main():
+def auto_simulation():
     cwd = os.getcwd()
     plan_path = f"{cwd}/simulation_plan.csv"
     plan = pd.read_csv(plan_path)
@@ -39,6 +41,67 @@ def main():
         plt.close('all')
         gc.collect()
 
+def auto_analysis():
+    cwd = os.getcwd()
+    plan_path = f"{cwd}/analyse_plan.csv"
+    plan = pd.read_csv(plan_path)
+    # Ensure expected columns
+    if "done" not in plan.columns:
+        plan["done"] = False
+
+    # Process sequentially instead of DataFrame.apply
+    to_run_idx = plan.index[~plan["done"]]
+
+    for idx in to_run_idx:
+        row = plan.loc[idx]
+        diff = row["diffusion"]
+        k_off = row["k_off"]
+        run_sweeping_eff(diff, k_off)
+        # Mark as done and persist after each row
+        plan.at[idx, "done"] = True
+        plan.to_csv(plan_path, index=False)
+
+        # Free memory between runs
+        plt.close('all')
+        gc.collect()
+
+def create_results():
+    cwd = os.getcwd()
+    test_dir = f"{cwd}/test/"
+    results_df = pd.DataFrame()
+    for folder in os.listdir(test_dir):
+        for file in os.listdir(test_dir+folder):
+            if "sweeping_eff.csv" in file:
+                df = pd.read_csv(test_dir+folder+"/"+file)
+                df = df.drop(df.columns[0], axis=1)
+                results_df = pd.concat([results_df, df])
+            else:
+                continue
+    # Drop rows with specific diffusion values
+    results_df = results_df[~results_df['diffusion'].isin([0.03, 0.05, 0.1])]
+    # Pivot the dataframe
+    pivot_df = results_df.pivot(index='k_off', columns='diffusion', values='sweeping efficiency')
+
+    # Create the plot with square cells
+    fig, ax = plt.subplots(figsize=(8, 8), dpi=200)
+    im = ax.imshow(pivot_df.values, aspect='auto', cmap='viridis', origin='lower')
+    plt.colorbar(im, label='Sweeping Efficiency')
+
+    # Set tick labels to actual values
+    ax.set_xticks(np.arange(len(pivot_df.columns)))
+    ax.set_xticklabels(pivot_df.columns)
+    ax.set_yticks(np.arange(len(pivot_df.index)))
+    ax.set_yticklabels(pivot_df.index)
+
+    ax.set_xlabel('Diffusion')
+    ax.set_ylabel('k_off')
+    ax.set_title('Sweeping Efficiency')
+    plt.tight_layout()
+    plt.show()
+    plt.savefig(f"results/sweeping_eff.png")
+    return
 
 if __name__ == "__main__":
-    main()
+    auto_simulation()
+    auto_analysis()
+    create_results()
--- new_analyse_update_290925.py
+++ new_analyse_update_290925.py
@@ -21,232 +21,117 @@ import logging
 import os
 import sys
 
-# local library
-dir_local = os.path.dirname(__file__)
-sys.path.append(dir_local)
-lib_path = "/home/cedrik/Documents/filaments-crosslinkers-projects/lib"
-sys.path.append(lib_path)
+
+def commacolon(x):
+    s = str(x)
+    return s.replace('.', ',') if '.' in s else s
+
+def run_sweeping_eff(diff, activity):
+
+    name = f"Braun2011_diff_{commacolon(diff)}_chem_{commacolon(activity)}"
+    name_input_file = name+'_s1'
+    extension_input_file = '.h5'
+    working_path = os.path.dirname(__file__)
+    folder = f"{working_path}/test/{name}"
+    dir_input_file = f"{folder}/"
+    dir_output_file = folder
+    name_output_file = f"output_{name}"
+
+    bool_anim = False
+
+    # print(name_save)
 
 
-logger = logging.getLogger(__name__)
-import lib_simulation as LS
+    tasks = d3.load_tasks_to_xarray(dir_input_file+name_input_file+extension_input_file) # Downloadig the files
+
+    x_tasks = np.array(tasks['f_A']['x'])
+    t_tasks = np.array(tasks['f_A']['t'])/60
+
+    n_img = 2
+    N_end = len(t_tasks)
+    extension_animation = ".gif"
+    frame_per_second = 20
+
+    if bool_anim:
+        fig = plt.figure(figsize=(5, 2), dpi=200)
+        def animate(i):
+            if i%(N_end/100) == 0:
+                print(i)
+            plt.clf()
+            plt.plot(x_tasks,tasks['f_A'][i], color='blue',alpha = 0.5, label = r"$\phi^A$")
+            plt.plot(x_tasks,tasks['f_B'][i], color='red',alpha = 0.5, label = r"$\phi^B$")
+
+            plt.plot(x_tasks,tasks['Pab'][i],color = 'violet',linestyle="-",label = r"$P^{ab}$")
+            plt.plot(x_tasks,tasks['Pa'][i],color = 'blue',linestyle="-",label = r"$P^{a}$")
+            plt.plot(x_tasks,tasks['Pb'][i],color = 'red',linestyle="-",label = r"$P^{b}$")
+
+            plt.plot(x_tasks,tasks['Mab'][i],color = 'violet',linestyle="--",label = r"$P^{ab}$")
+            plt.plot(x_tasks,tasks['Ma'][i],color = 'blue',linestyle="--",label = r"$P^{a}$")
+            plt.plot(x_tasks,tasks['Mb'][i],color = 'red',linestyle="--",label = r"$P^{b}$")
+
+            # plt.plot(x_tasks,tasks['f_D'][i]-tasks['n_D'][i], color='red',alpha = 0.5, label = r"$\phi^B$")
+
+            plt.legend(loc='upper left',fontsize=5)
 
 
+        t=np.arange(0,N_end,n_img) # New time array with only n images
+        ani = FuncAnimation(fig, animate, frames=t,
+                            interval=1, repeat=False)
+        #name = "D"+str(D)+"a"+str(alpha)+".gif"
+        ani.save(dir_output_file+"/"+name_output_file+"_density"+extension_animation, writer = 'ffmpeg', fps = frame_per_second)
+
+    Overlap = np.zeros(len(t_tasks))
+
+    N_Pab = np.zeros(len(t_tasks))
 
 
+    for i in range(len(t_tasks)):
+        N_Pab[i] = integrate.simpson(tasks['Pab'][i],x_tasks)
+        Overlap[i] = integrate.simpson(tasks['f_D'][i],x_tasks)
 
-#%%
-dir_local = os.path.dirname(__file__)
+    for t in range(len(tasks['Pab'])):
+        tasks['Pab'][t][tasks['Pab'][t]<=0] = 0
+        tasks['f_B'][t][tasks['f_B'][t]<=0] = 0
+        tasks['f_A'][t][tasks['f_A'][t]<=0] = 0
 
-name =    "Braun2011_diff_0,5_chem_15,0"
-dir_input_file = '/home/cedrik/Documents/FCI-ExplorationProject/__pycache__/FCI-ExplorationProject-main_ben/'+name+'/'
-name_input_file = name+'_s1'
-extension_input_file = '.h5'
+    A = np.zeros((len(t_tasks),len(x_tasks),3))
 
-dir_output_file = dir_local
-name_output_file = "output_test1"
+    A[:,:,1]= 0+0.8*tasks['Pab']/np.max(tasks['Pab'])
+    A[:,:,0]= 0+0.5*tasks['f_B']/np.max(tasks['f_B']) + 0.3*tasks['f_A']/np.max(tasks['f_A'])
 
+    """
+    FROM HERE I AM TESTING STUFF
+    """
 
-bool_anim = 0
-
-# print(name_save)
-
-
-
-
-#%% loading data
-tasks = d3.load_tasks_to_xarray(dir_input_file+name_input_file+extension_input_file) # Downloadig the files
-
-x_tasks = np.array(tasks['f_A']['x'])
-t_tasks = np.array(tasks['f_A']['t'])/60
-
-# x_tasks = np.array(tasks['n_Mab']['x'])
-# t_tasks = np.array(tasks['n_Mab']['t'])/60
-
-#%%
-n_img = 2
-N_end = len(t_tasks)
-extension_animation = ".gif"
-frame_per_second = 20
+    def func_linear(x,E):
+        return E*x
 
 
-# %% ANIMATION
-if bool_anim:
-    fig = plt.figure(figsize=(5, 2), dpi=200)
-    def animate(i):
-        if i%(N_end/100) == 0:
-            print(i)
-        plt.clf()
-        plt.plot(x_tasks,tasks['f_A'][i], color='blue',alpha = 0.5, label = r"$\phi^A$")
-        plt.plot(x_tasks,tasks['f_B'][i], color='red',alpha = 0.5, label = r"$\phi^B$")
-        
-        plt.plot(x_tasks,tasks['Pab'][i],color = 'violet',linestyle="-",label = r"$P^{ab}$")
-        plt.plot(x_tasks,tasks['Pa'][i],color = 'blue',linestyle="-",label = r"$P^{a}$")
-        plt.plot(x_tasks,tasks['Pb'][i],color = 'red',linestyle="-",label = r"$P^{b}$")
+    def func_power(x,E):
+        return np.power(x,E)
 
-        plt.plot(x_tasks,tasks['Mab'][i],color = 'violet',linestyle="--",label = r"$P^{ab}$")
-        plt.plot(x_tasks,tasks['Ma'][i],color = 'blue',linestyle="--",label = r"$P^{a}$")
-        plt.plot(x_tasks,tasks['Mb'][i],color = 'red',linestyle="--",label = r"$P^{b}$")
+    ib = 1 # first point
+    ie = 50 # last point
 
-        # plt.plot(x_tasks,tasks['f_D'][i]-tasks['n_D'][i], color='red',alpha = 0.5, label = r"$\phi^B$")
+    x = Overlap[ib]/Overlap[ib:ie]
+    y = (N_Pab[ib:ie]/Overlap[ib:ie])/(N_Pab[ib]/Overlap[ib])
+    popt2, pcov2 =scipy.optimize.curve_fit(func_power, x, y)
 
-        plt.legend(loc='upper left',fontsize=5)
-    
-        
-    t=np.arange(0,N_end,n_img) # New time array with only n images  
-    ani = FuncAnimation(fig, animate, frames=t,
-                        interval=1, repeat=False)
-    #name = "D"+str(D)+"a"+str(alpha)+".gif"
-    ani.save(dir_output_file+"/"+name_output_file+"_density"+extension_animation, writer = 'ffmpeg', fps = frame_per_second)
+    plt.figure(dpi=200,figsize=(8,8))
+    sweeping_eff = popt2[0]
+    plt.title(f"Sweeping efficiency = {str(sweeping_eff)}")
+    plt.plot(Overlap[ib]/Overlap[ib:ie],(N_Pab[ib:ie]/Overlap[ib:ie])/(N_Pab[ib]/Overlap[ib]),linewidth=3)
 
+    plt.plot(x,func_power(x,popt2[0]))
 
+    plt.xscale('log')
+    plt.yscale('log')
+    plt.show()
 
-# %% ANIMATION
-# if bool_anim:
-#     fig = plt.figure(figsize=(5, 2), dpi=200)
-#     def animate(i):
-#         if i%(N_end/100) == 0:
-#             print(i)
-#         plt.clf()    
-#         plt.plot(x_tasks,tasks['S_el_xx'][i],linestyle="-",label = r"$S^{el}_{xx}$")
-#         plt.plot(x_tasks,tasks['S_el_xy'][i],linestyle="-",label = r"$S^{el}_{xy}$")
-#         plt.plot(x_tasks,tasks['S_el_yx'][i],linestyle="-",label = r"$S^{el}_{yx}$")
-#         plt.plot(x_tasks,tasks['S_el_yy'][i],linestyle="-",label = r"$S^{el}_{yy}$")
-#         # plt.plot(x_tasks,tasks['S_el_xx'][i]-tasks['S_el_yy'][i],linestyle="-",label = r"$S^{el}_{yy}$")
-
-        
-        
-#         # plt.plot(x_tasks,tasks['f_A'][i], color='blue',alpha = 0.5, label = r"$\phi^A$")
-#         # plt.plot(x_tasks,tasks['f_B'][i], color='red',alpha = 0.5, label = r"$\phi^B$")
-#         # plt.plot(x_tasks,tasks['f_D'][i]-tasks['n_D'][i], color='red',alpha = 0.5, label = r"$\phi^B$")
-#         plt.ylim(-5000,5000)
-#         plt.legend(loc='upper left')
-    
-        
-#     t=np.arange(0,N_end,n_img) # New time array with only n images  
-#     ani = FuncAnimation(fig, animate, frames=t,
-#                         interval=1, repeat=False)
-#     #name = "D"+str(D)+"a"+str(alpha)+".gif"
-#     ani.save(dir_output_file+"/"+name_output_file+"_stresses"+extension_animation, writer = 'ffmpeg', fps = frame_per_second)
-
-#%% Force calculation
-
-
-
-# FA_vis = np.zeros(len(t_tasks))
-# FA_fri = np.zeros(len(t_tasks))
-# FA_ent = np.zeros(len(t_tasks))
-# FA_ela = np.zeros(len(t_tasks))
-# FA_act = np.zeros(len(t_tasks))
-
-# FB_vis = np.zeros(len(t_tasks))
-# FB_fri = np.zeros(len(t_tasks))
-# FB_ent = np.zeros(len(t_tasks))
-# FB_ela = np.zeros(len(t_tasks))
-# FB_act = np.zeros(len(t_tasks))
-
-# VA = np.zeros(len(t_tasks))
-# VB = np.zeros(len(t_tasks))
-
-Overlap = np.zeros(len(t_tasks))
-
-N_Pab = np.zeros(len(t_tasks))
-
-
-for i in range(len(t_tasks)):
-    # FA_vis[i] = tasks['F_fA_vis'][i][10]
-    # FA_fri[i] = tasks['F_fA_fri'][i][10]
-    # FA_ent[i] = tasks['F_fA_ent'][i][10]
-    # FA_ela[i] = tasks['F_fA_ela'][i][10]
-    # FA_act[i] = tasks['F_fA_act'][i][10]
-    
-    # FB_vis[i] = tasks['F_fB_vis'][i][10]
-    # FB_fri[i] = tasks['F_fB_fri'][i][10]
-    # FB_ent[i] = tasks['F_fB_ent'][i][10]
-    # FB_ela[i] = tasks['F_fB_ela'][i][10]
-    # FB_act[i] = tasks['F_fB_act'][i][10]
-    
-    N_Pab[i] = integrate.simpson(tasks['Pab'][i],x_tasks)
-    # NB[i] = integrate.simpson(tasks['Pb'][i])
-    # NAB[i] = integrate.simpson(tasks['Pab'][i]) 
- 
-    # VA[i] = tasks['V_A'][i][10]
-    # VB[i] = tasks['V_B'][i][10]
-    
-    Overlap[i] = integrate.simpson(tasks['f_D'][i],x_tasks)
-
-#%%
-
-
-#%%
-for t in range(len(tasks['Pab'])):
-    tasks['Pab'][t][tasks['Pab'][t]<=0] = 0
-    tasks['f_B'][t][tasks['f_B'][t]<=0] = 0
-    tasks['f_A'][t][tasks['f_A'][t]<=0] = 0
-
-
-#%%
-A = np.zeros((len(t_tasks),len(x_tasks),3))
-# for t in range(len(t_tasks)):
-#     print(t)
-#     A[t] = tasks['n_D'][t]
-#     for i in range(len(x_tasks)):
-#         if A[t][i]<=0.021:
-#             A[t][i] = -0.1*tasks['f_B'][t][i]
-
-A[:,:,1]= 0+0.8*tasks['Pab']/np.max(tasks['Pab'])     
-A[:,:,0]= 0+0.5*tasks['f_B']/np.max(tasks['f_B']) + 0.3*tasks['f_A']/np.max(tasks['f_A'])    
-
-#
-#%%
-plt.figure(dpi = 200,figsize= (4,6))
-# plt.pcolormesh(x_tasks,-t_tasks, tasks['n_D'])
-plt.pcolormesh(x_tasks,t_tasks,A)
-plt.gca().invert_yaxis()
-plt.ylabel("time (min)")
-plt.xlabel("distance to template filament end ($\mu$m)")
-plt.show()
-
-
-
-#%%
-"""
-FROM HERE I AM TESTING STUFF
-"""
-
-def func_linear(x,E):
-    return E*x
-    
-    
-def func_power(x,E):
-    return np.power(x,E)   
-    
-
-
-#%%
-
-ib = 1 # first point
-ie = 50 # last point
-
-x = Overlap[ib]/Overlap[ib:ie]
-y = (N_Pab[ib:ie]/Overlap[ib:ie])/(N_Pab[ib]/Overlap[ib])
-popt2, pcov2 =scipy.optimize.curve_fit(func_power, x, y)
-
-plt.figure(dpi=200,figsize=(8,8))
-plt.title("Sweeping efficiency = "+str(popt2[0]))
-plt.plot(Overlap[ib]/Overlap[ib:ie],(N_Pab[ib:ie]/Overlap[ib:ie])/(N_Pab[ib]/Overlap[ib]),linewidth=3)
-# plt.plot(Overlap[ib]/Overlap[ib:ie],np.power(Overlap[ib]/Overlap[ib:ie],E))
-
-# plt.plot(x,func_linear(x,E))
-plt.plot(x,func_power(x,popt2[0]))
-
-
-
-# plt.ylim(0,1)
-# plt.xlim(0,5.1)
-plt.xscale('log')
-plt.yscale('log')
-plt.show()
+    plt.savefig(os.path.join(dir_input_file, f"{name_output_file}_sweeping_eff.png"), dpi=200)
+    df = pd.DataFrame({'diffusion':[diff],'k_off':[activity],'sweeping efficiency':[sweeping_eff]})
+    df.to_csv(os.path.join(dir_input_file, f"{name_output_file}_sweeping_eff.csv"))
+    return
 
 #%%
 #%%
